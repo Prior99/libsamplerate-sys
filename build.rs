f64b5e1814f386b12a49c639d75b3aa70bb88b42
Aaron Griffith <aargri@gmail.com>
2020-06-26 12:22:30 -0400

    Use cmake crate for building libsamplerate

--- build.rs
+++ build.rs
@@ -1,67 +1,14 @@
 extern crate bindgen;
-extern crate pathdiff;
+extern crate cmake;
 
-use pathdiff::diff_paths;
 use std::env;
-use std::fs;
 use std::path::PathBuf;
-use std::process::Command;
-
-fn run(cmd: &mut Command) {
-    match cmd.status() {
-        Ok(status) => assert!(status.success()),
-        Err(e) => panic!("Unable to execute {:?}! {}", cmd, e),
-    }
-}
-
-fn build_libsamplerate_unix(src: &PathBuf, dst: &PathBuf) {
-    run(Command::new("cmake")
-        .args(&[
-            "-DCMAKE_C_FLAGS=-fPIC",
-            &format!(
-                "-DCMAKE_BUILD_TYPE={}",
-                if env::var("PROFILE").map(|x| x == "release").unwrap_or(false) {
-                    "Release"
-                } else {
-                    "Debug"
-                }
-            ),
-            diff_paths(&src, &dst).unwrap().to_str().unwrap(),
-        ])
-        .current_dir(&dst));
-    run(Command::new("make").args(&["samplerate"]).current_dir(&dst));
-    let shlib = src.join("src/.libs");
-    let _ = fs::copy(&shlib.join("libsamplerate.a"), &dst.join("libsamplerate.a"));
-    println!("cargo:rustc-flags=-l static=samplerate");
-    println!("cargo:rustc-flags=-L {}", dst.display());
-    println!("cargo:rerun-if-changed={}", src.to_str().unwrap());
-}
-
-fn build_libsamplerate_windows(src: &PathBuf, dst: &PathBuf) {
-    run(Command::new("cmake").args(&[
-        "-DCMAKE_SYSTEM_NAME=Windows",
-        "-DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc",
-        "-DCMAKE_C_FLAGS=-fPIC",
-        "-DBUILD_SHARED_LIBS=off",
-        "-DWIN32=on",
-        "-DSNDFILE_INCLUDE_DIR=/usr/include",
-        "-DBUILD_EXAMPLES=OFF",
-        diff_paths(&src, &dst).unwrap().to_str().unwrap(),
-    ]).current_dir(&dst));
-    run(Command::new("make").args(&["samplerate"]).current_dir(&dst));
-    println!("cargo:rustc-flags=-l static=samplerate");
-    println!("cargo:rustc-flags=-L {}", dst.display());
-    println!("cargo:rerun-if-changed={}", src.to_str().unwrap());
-}
 
 fn main() {
-    let src = PathBuf::from(&env::var_os("CARGO_MANIFEST_DIR").unwrap()).join("libsamplerate");
-    let dst = PathBuf::from(&env::var_os("OUT_DIR").unwrap());
-    let _ = fs::create_dir(&dst);
-    match env::var("CARGO_CFG_TARGET_OS").as_ref().map(|x| &**x) {
-        Ok("windows") => build_libsamplerate_windows(&src, &dst),
-        _ => build_libsamplerate_unix(&src, &dst),
-    };
+    let libsr = cmake::build("libsamplerate");
+    println!("cargo:rustc-link-search=native={}", libsr.join("lib").display());
+    println!("cargo:rustc-link-lib=static=samplerate");
+
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .generate()
