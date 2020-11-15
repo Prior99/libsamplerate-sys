c82a58a0c5d12adefd4c9882312ea369b1287fee
Matthias Geier <Matthias.Geier@gmail.com>
2020-11-15 12:32:57 +0100

    build.rs: use build_target()
    Fixes #8.

--- build.rs
+++ build.rs
@@ -5,8 +5,17 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let libsr = cmake::build("libsamplerate");
-    println!("cargo:rustc-link-search=native={}", libsr.join("lib").display());
+    let libsr = cmake::Config::new("libsamplerate")
+        .build_target("samplerate")
+        .build();
+    let path = libsr.join("build");
+
+    println!("cargo:rustc-link-search=native={}", path.display());
+    // Debug/Release/... paths for MSVC:
+    println!("cargo:rustc-link-search=native={}", path.join("Debug").display());
+    println!("cargo:rustc-link-search=native={}", path.join("Release").display());
+    println!("cargo:rustc-link-search=native={}", path.join("RelWithDebInfo").display());
+    println!("cargo:rustc-link-search=native={}", path.join("MinSizeRel").display());
     println!("cargo:rustc-link-lib=static=samplerate");
 
     let bindings = bindgen::Builder::default()
