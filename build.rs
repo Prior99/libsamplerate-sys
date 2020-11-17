056d5d91d7dcc3676ca56022e97f8029e9fd8964
Matthias Geier <Matthias.Geier@gmail.com>
2020-11-17 12:09:35 +0100

    Pre-generate bindings.rs

--- build.rs
+++ build.rs
@@ -1,20 +1,7 @@
-extern crate bindgen;
 extern crate cmake;
-
-use std::env;
-use std::path::PathBuf;
 
 fn main() {
     let libsr = cmake::build("libsamplerate");
     println!("cargo:rustc-link-search=native={}", libsr.join("lib").display());
     println!("cargo:rustc-link-lib=static=samplerate");
-
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .generate()
-        .expect("Unable to generate bindings");
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
 }
